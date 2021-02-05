82b87bdb74295e20fd9c6e8a8408806a37f19a87
Angelita Santos <angelitasantos@gmail.com>
2021-02-05 10:15:38 -0300

    created controllers companies, positions and applicants

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :companies, only: [:new, :edit, :create, :update]
+  resources :positions
+  resources :applicants, only: [:index, :new, :create]
   devise_for :users
   root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
