cd79051a72af9b58c72a17e1867f03cdff64d241
angelitasantos <angelitasantos@gmail.com>
2021-02-08 16:31:13 -0300

    created repository

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,14 @@ Rails.application.routes.draw do
   resources :applicants, only: [:index, :new, :create]
   devise_for :users
   root to: 'home#index'
-  get 'simulador', to: 'home#simulador'
-  get 'financeiro', to: 'home#financeiro'
-  get 'vendas', to: 'home#vendas'
-  get 'cadastros', to: 'home#cadastros'
-  get 'operacional', to: 'home#operacional'
-  get 'relatorios', to: 'home#relatorios'
-  get 'painel', to: 'home#painel'
-  get 'site', to: 'home#site'
+  get 'painel', to: 'painel#index'
+  get 'simulador', to: 'painel#simulador'
+  get 'financeiro', to: 'painel#financeiro'
+  get 'vendas', to: 'painel#vendas'
+  get 'cadastros', to: 'painel#cadastros'
+  get 'operacional', to: 'painel#operacional'
+  get 'relatorios', to: 'painel#relatorios'
+  get 'painelconfig', to: 'painel#painel'
+  get 'siteconfig', to: 'painel#site'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
