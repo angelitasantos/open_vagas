67d25f7f8939dbfda8c74f11de465788909bcfef
angelitasantos <angelitasantos@gmail.com>
2021-02-08 15:35:10 -0300

    created painel

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,13 @@ Rails.application.routes.draw do
   resources :applicants, only: [:index, :new, :create]
   devise_for :users
   root to: 'home#index'
+  get 'simulador', to: 'home#simulador'
+  get 'financeiro', to: 'home#financeiro'
+  get 'vendas', to: 'home#vendas'
+  get 'cadastros', to: 'home#cadastros'
+  get 'operacional', to: 'home#operacional'
+  get 'relatorios', to: 'home#relatorios'
+  get 'painel', to: 'home#painel'
+  get 'site', to: 'home#site'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
